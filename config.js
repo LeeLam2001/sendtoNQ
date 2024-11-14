a4d716a59e552511adc441f181a1469666edba19
Lê Văn Lâm <87559387+LeeLam2001@users.noreply.github.com>
2024-11-14 20:54:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Ê nhóc :v ',
+    introTitle: 'Hiluuu bạn nhỏ NQ :v ',
     introDesc: `Trái đất vốn lạ thường,
     Mà sao em cứ đi nhầm đường,
     Lạc vào tim anh lẻ loi,
