50029746d2d612ebda7f04d9d38279f5f69f86e1
Lê Văn Lâm <87559387+LeeLam2001@users.noreply.github.com>
2025-03-07 22:58:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Hilooo bạn nhỏ NQ ',
+    introTitle: 'Hilooo cậu yêu ',
     introDesc: `Trái đất vốn lạ thường,
     Mà sao em cứ đi nhầm đường,
     Lạc vào tim anh lẻ loi,
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'Ấn vô tiếp đi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là yêu anh <33',
+    btnYes: 'Vẫn cứ là yêu cậu <33',
     btnNo: 'Không chọn được cái này đâu lew lew ^^ :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Bởi vì em yêu anh <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Yêu em nhiều lắm nhó <333.',
+    reply: 'Bởi vì tớ yêu cậu vậy thôi <33333333',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Yêu cậu nhiều lắm nhó <333.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://m.me/lee21201' 
 }
